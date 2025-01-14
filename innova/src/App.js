0a18512f4fe3bd4a1960897ce79a89e38205c199
pannzzz <mariazzz2326@gmail.com>
2025-01-13 23:38:28 -0500

    se crea el login y el registro

--- innova/src/App.js
+++ innova/src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 // Screens
 import Landing from "./screens/Landing.jsx";
+// Rutas
+import Login from "./components/Elements/Login";
+import Register from "./components/Elements/Register";
 
 export default function App() {
   return (
-    <>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap" rel="stylesheet" />
-      </Helmet>
-      <Landing />
-    </>
+    <Router>
+      <>
+        <Helmet>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Khula:wght@400;600;800&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
-
--- innova/src/components/Nav/Sidebar.jsx
+++ innova/src/components/Nav/Sidebar.jsx
@@ -106,12 +106,12 @@ export default function Sidebar({ sidebarOpen, toggleSidebar }) {
       </UlStyle>
       <UlStyle className="flexSpaceCenter">
         <li className="semiBold font15 pointer">
-          <a href="/" style={{ padding: "10px 30px 10px 0" }} className="whiteColor">
+          <a href="/login" style={{ padding: "10px 30px 10px 0" }} className="whiteColor">
             Log in
           </a>
         </li>
         <li className="semiBold font15 pointer flexCenter">
-          <a href="/" className="radius8 lightBg" style={{ padding: "10px 15px" }}>
+          <a href="/register" className="radius8 lightBg" style={{ padding: "10px 15px" }}>
             Register here 
           </a>
         </li>
